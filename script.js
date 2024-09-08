195d7a9fa8e09b26ff55c2986d7e1af88cdd58fb
Isasantamaria <isabelasantarendon@gmail.com>
2024-09-08 17:49:48 -0500

    Declaro una funcion para la peticion de la api, para podermela traer y que me proporcione la información de los paises

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const obtenerValorInput = () => {
     let valor = inputTexto.value
     peticionesApi(valor)
 }
+
+const peticionesApi = (pais) => {
+    const baseUrl = 'https://restcountries.com/v3.1/'
+    const enpoint = `name/${pais}`
+    const url = `${baseUrl}${enpoint}`
+
+    axios.get(url)
+    .then(res => printData(res.data))
+    .catch(err => console.log(err))
+}
